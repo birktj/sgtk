dd99ff955d72de6fadf9761634fbe46a97af40a5
Birk Tjelmeland <git@birktj.no>
2021-04-11 18:16:17 +0100

    Add toroidal bench

---
+++ benches/toroidal.rs
@@ -0,0 +1,26 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::{black_box, Criterion};
+use sgtk::graph::{minors, Graph, Graph16};
+
+pub fn benchmark(c: &mut Criterion) {
+    c.bench_function("toroidal_k7", |b| b.iter(|| {
+        sgtk::toroidal::find_embedding(&black_box(Graph16::complete(7))).is_some()
+    }));
+
+    let obstruction = sgtk::parse::from_upper_tri("9 111000011100001100001000011111111111")
+        .unwrap();
+    c.bench_function("toroidal_obstruction", |b| b.iter(|| {
+        sgtk::toroidal::find_embedding(&black_box(obstruction)).is_some()
+    }));
+    c.bench_function("toroidal_obstruction_check_minors", |b| b.iter(|| {
+        minors(&black_box(obstruction))
+            .filter(|minor| minor.is_connected())
+            .filter(|minor| sgtk::toroidal::find_embedding(&minor).is_some())
+            .count()
+    }));
+}
+
+criterion_group!(benches, benchmark);
+criterion_main!(benches);
