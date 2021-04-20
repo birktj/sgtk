4c8c312707c45eae908ddc0d3bdcbda51fbe8ca3
Birk Tjelmeland <git@birktj.no>
2021-04-20 16:37:36 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+tar --transform "s/^/sgtk\//" -cf sgtk.tar.gz src Cargo.toml .cargo tests ffi/src ffi/Cargo.toml ffi/.cargo f4m/src f4m/Cargo.toml f4m/.cargo geng-search
+scp sgtk.tar.gz bt399@login-cpu.hpc.cam.ac.uk:~/toroidal-search
+
+ssh bt399@login-cpu.hpc.cam.ac.uk <<EOF
+cd toroidal-search
+tar -xf sgtk.tar.gz
+cd sgtk/f4m
+cargo build --release
+cd ../ffi
+cargo build --release
+cd ../geng-search
+make toroidal-geng
+EOF
